e8dd7866d70d854fe4680f83eb0640af1854014c
Brian <bstanton773@gmail.com>
2022-08-15 14:50:11 -0500

    Jinja Templating

--- app/routes.py
+++ app/routes.py
@@ -8,9 +8,10 @@ def index():
         'username': 'brians',
         'email': 'brians@codingtemple.com'
     }
-    return render_template('index.html',user=user_info)
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    return render_template('index.html',user=user_info, colors=colors)
 
 
 @app.route('/test')
 def test():
-    return 'This is a test'
+    return '<h1>This is a test</h1>'
