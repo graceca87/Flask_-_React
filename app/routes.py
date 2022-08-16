3adf5708fcf8f98b8c1fed780c08634cfa44cd3a
Brian <bstanton773@gmail.com>
2022-08-16 11:46:50 -0500

    Change test route to signup route

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,6 @@ def index():
     return render_template('index.html',user=user_info, colors=colors)
 
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
